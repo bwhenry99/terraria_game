db4bd0e067f9167f6908709ef164ccc4445836b8
bwhenry99 <bwhenry19@gmail.com>
2026-05-23 17:42:01 -0600

    properly link raylib, create first window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <raylib.h>
 
 int main()
 {
-    std::cout << "Hello World!";
+    SetConfigFlags(ConfigFlags::FLAG_WINDOW_RESIZABLE);
+    InitWindow(512, 256, "my window");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawText("Hello Window", 128, 128, 24, RED);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
