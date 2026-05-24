32a06b7490d7da461063120c5b8bd8c4635ac0c9
bwhenry99 <bwhenry19@gmail.com>
2026-05-24 08:36:08 -0600

    add and link imgui and rlimgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 #include <raylib.h>
+#include <imgui.h>
+#include <rlImGui.h>
 
 int main()
 {
     SetConfigFlags(ConfigFlags::FLAG_WINDOW_RESIZABLE);
     InitWindow(512, 256, "my window");
+    rlImGuiSetup(true);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        rlImGuiBegin();
+
+        DrawRectangle(75, 75, 100, 100, { 0, 255, 0, 255 });
+        DrawRectangle(50, 50, 100, 100, { 255, 0, 0, 255 });
 
         DrawText("Hello Window", 128, 128, 24, RED);
 
+        ImGui::Begin("test");
+        ImGui::Text("Hello from imgui");
+        ImGui::End();
+
+        rlImGuiEnd();
         EndDrawing();
     }
 
+    rlImGuiShutdown();
     CloseWindow();
     return 0;
 }
