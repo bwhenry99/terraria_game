4d01b3e153b72d44bc51614d96dbf299de83135e
bwhenry99 <bwhenry19@gmail.com>
2026-09-20 10:47:42 -0600

    set up imgui docking

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,23 @@ int main()
 {
     SetConfigFlags(ConfigFlags::FLAG_WINDOW_RESIZABLE);
     InitWindow(512, 256, "my window");
-    rlImGuiSetup(true);
 
+    rlImGuiSetup(true);
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+    float property = 50.0f;
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         rlImGuiBegin();
+
+        // docking config 
+        ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
+        ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
+        ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+        ImGui::PopStyleColor(2);
 
         DrawRectangle(75, 75, 100, 100, { 0, 255, 0, 255 });
         DrawRectangle(50, 50, 100, 100, { 255, 0, 0, 255 });
@@ -22,6 +32,19 @@ int main()
 
         ImGui::Begin("test");
         ImGui::Text("Hello from imgui");
+        if (ImGui::Button("button"))
+        {
+           std::cout << property << std::endl;
+        }
+
+        ImGui::SliderFloat("slider", &property, 0, 100);
+        ImGui::End();
+
+        ImGui::Begin("window2");
+        if (ImGui::Button("button"))
+        {
+           std::cout << "hello from window 2" << std::endl;
+        }
         ImGui::End();
 
         rlImGuiEnd();
